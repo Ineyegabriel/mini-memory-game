10e93416b70dfda0079e09d926910ec74e81dc59
= <=>
2021-02-07 20:51:12 +0000

    fixing scores display

--- src/Pages/ScorePage.js
+++ src/Pages/ScorePage.js
@@ -53,7 +53,7 @@ const ScorePage = memo(({username, score, highscore}) => {
             <span className="highscore">
               <div className="highScoreBadge">
                 <Trophy className="highscoreLogo" />
-                <span className="score">{Math.round(highscore.score)}</span>
+                <span className="score">{Math.floor(highscore.score)}</span>
               </div>
 
               <span className="playerDetails">
@@ -66,7 +66,7 @@ const ScorePage = memo(({username, score, highscore}) => {
             <span className="highscore">
               <div className="highScoreBadge">
                 <Man className="highscoreLogo" />
-                <span className="score">{Math.round(score)}</span>
+                <span className="score">{Math.floor(score)}</span>
               </div>
 
               <span className="playerDetails">
